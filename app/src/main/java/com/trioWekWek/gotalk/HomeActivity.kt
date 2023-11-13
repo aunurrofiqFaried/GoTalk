42d3e988154835836a67333187c694477fce8abd
Amigo Wahyu <amigo2018wahyu@gmail.com>
2023-11-13 08:56:27 +0700

    Membuat halaman login

--- app/src/main/java/com/trioWekWek/gotalk/HomeActivity.kt
+++ app/src/main/java/com/trioWekWek/gotalk/HomeActivity.kt
@@ -2,7 +2,7 @@ package com.trioWekWek.gotalk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/trioWekWek/gotalk/SignUpActivity.kt
+++ app/src/main/java/com/trioWekWek/gotalk/SignUpActivity.kt
@@ -11,7 +11,7 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.trioWekWek.gotalk.HomeActivity
 import com.trioWekWek.gotalk.databinding.ActivitySignUpBinding
-//
+
 class SignUpActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
