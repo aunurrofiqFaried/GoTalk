dab67de38eaf2a682de17988ea5e93d57f35496d
Senoes <senosatrio46@gmail.com>
2023-11-22 07:30:09 +0700

    Kode Program mengatur Splash screen

---
+++ app/src/main/java/com/trioWekWek/gotalk/SplashscreenActivity.kt
@@ -0,0 +1,26 @@
+package com.trioWekWek.gotalk
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+@SuppressLint("CustomSplashScreen")
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.splash_screen)
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            startActivity(Intent(this,LoginActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
